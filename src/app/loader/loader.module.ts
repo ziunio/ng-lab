fc13816c6983745ebe03916e5f51c8001d17d4c8
Wojciech Chociej <chociej.w@gmail.com>
2020-05-19 23:51:35 +0200

    loader

---
+++ src/app/loader/loader.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { WithLoadingPipe } from "./with-loading.pipe";
+
+@NgModule({
+  declarations: [WithLoadingPipe],
+  imports: [CommonModule],
+})
+export class LoaderModule {}
