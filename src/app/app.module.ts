75ba560f2df1f7bc1ccec3a00418a1c2134819e8
Wojciech Chociej <chociej.w@gmail.com>
2020-07-27 19:27:25 +0200

    feat(threejs): add examples (grid, object)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoaderModule } from "./loader/loader.module";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, LoaderModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/three-js/three-js.module.ts
+++ src/app/three-js/three-js.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from "@angular/common";
 import { ThreeJsRoutingModule } from "./three-js-routing.module";
 import { ThreeWrapperComponent } from "./three-wrapper/three-wrapper.component";
 import { AtftModule } from "atft";
+import { MeshComponent } from "./components/mesh/mesh.component";
+import { ObjectComponent } from "./components/object/object.component";
 
 @NgModule({
-  declarations: [ThreeWrapperComponent],
+  declarations: [ThreeWrapperComponent, MeshComponent, ObjectComponent],
   imports: [CommonModule, ThreeJsRoutingModule, AtftModule],
 })
 export class ThreeJsModule {}
